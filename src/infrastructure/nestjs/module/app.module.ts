2f68be8560dfe4cd79f7ee9506ef2ea41a845875
TalesRG <talesrodriguesgoncalves98@gmail.com>
2025-08-13 22:45:17 -0300

    fix(auth): corrige importacao dos modulos

--- src/infrastructure/nestjs/module/app.module.ts
+++ src/infrastructure/nestjs/module/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import {ConfigModule, ConfigService} from "@nestjs/config";
 import {TypeOrmModule} from "@nestjs/typeorm";
 import databaseConfig from "../config/database.config";
+import {AuthModule} from "./auth.module";
 
 
 @Module({
   imports: [
+      AuthModule,
     ConfigModule.forRoot({
       isGlobal: true,
       load: [databaseConfig],
